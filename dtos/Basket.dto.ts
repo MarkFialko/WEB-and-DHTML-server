758ca0ef4a5804677969e77648e962cc0f6fa6e4
markfialko <markfialkofialko@gmail.com>
2023-11-04 00:07:34 +0500

    feat: basket types

--- dtos/Basket.dto.ts
+++ dtos/Basket.dto.ts
@@ -1,4 +1,3 @@
-import { Types } from 'mongoose'
 import { DishDTO } from './Dish.dto'
 
 export class BasketDishDto {
@@ -8,15 +7,5 @@ export class BasketDishDto {
   constructor(dish: DishDTO, count: number) {
     this.dish = dish
     this.count = count
-  }
-}
-
-export class BasketDto {
-  public id: Types.ObjectId
-  public dishes: BasketDishDto[]
-
-  constructor(basketId: Types.ObjectId, dishes: BasketDishDto[]) {
-    this.id = basketId
-    this.dishes = dishes
   }
 }
--- service/Basket.service.ts
+++ service/Basket.service.ts
@@ -3,9 +3,15 @@ import BasketDishSchema, { IBasketDishDocument } from '../models/BasketDish.sche
 import { OrderStatus } from '../models/Order.schema'
 import { BasketDishDto } from '../dtos/Basket.dto'
 import { DishDTO } from '../dtos/Dish.dto'
+import { Types } from 'mongoose'
+
+export interface BasketResponse {
+  basket: Types.ObjectId
+  dishes: BasketDishDto[]
+}
 
 class BasketService {
-  async getAll(userId: string) {
+  async getAll(userId: string): Promise<BasketResponse> {
     const userBasket = (await BasketSchema.findOne({
       user: userId
     })) as BasketDocument
@@ -17,7 +23,7 @@ class BasketService {
 
     type dishId = string
 
-    const basketDishesCount: Map<dishId,number> = new Map()
+    const basketDishesCount: Map<dishId, number> = new Map()
 
     const resultDishes: DishDTO[] = []
 
@@ -27,26 +33,25 @@ class BasketService {
       const dishDtoCount = basketDishesCount.get(dishDto.id)
 
       if (!dishDtoCount) {
-        basketDishesCount.set(dishDto.id,1)
+        basketDishesCount.set(dishDto.id, 1)
         resultDishes.push(dishDto)
       } else {
-        basketDishesCount.set(dishDto.id,dishDtoCount + 1)
+        basketDishesCount.set(dishDto.id, dishDtoCount + 1)
       }
-
     })
 
-      return {
-        basket: userBasket._id,
-        dishes: resultDishes.map((dish) => {
-          return {
-            ...dish,
-            count: basketDishesCount.get(dish.id)
-          }
-        })
-      }
+    return {
+      basket: userBasket._id,
+      dishes: resultDishes.map((dish) => {
+        return {
+          dish,
+          count: basketDishesCount.get(dish.id) as number
+        }
+      })
+    }
   }
 
-  async add(userId: string, dishId: string[]) {
+  async add(userId: string, dishId: string[]): Promise<BasketResponse> {
     const userBasket = (await BasketSchema.findOne({
       user: userId
     })) as BasketDocument
@@ -62,7 +67,7 @@ class BasketService {
     return await new BasketService().getAll(userId)
   }
 
-  async delete(userId: string, dishId: string[]) {
+  async delete(userId: string, dishId: string[]): Promise<BasketResponse> {
     const userBasket = (await BasketSchema.findOne({
       user: userId
     })) as BasketDocument
